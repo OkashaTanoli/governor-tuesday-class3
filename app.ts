b869402adbd50f60f656585295b733e06f18e089
okashatanoli <okashatanoli12345@gmail.com>
2024-02-28 03:35:54 +0500

    initial

---
+++ app.ts
@@ -0,0 +1,99 @@
+// ------------ Arithmetic Operators -------------
+
+// console.log(10+20); 
+
+// let num1 = 1;
+// let num2 = 5;
+// console.log(num1 + num2);
+//              1   +   5
+
+
+// let num1 = 7;
+// let num2 = 8;
+// let add = num1 + num2 + 10;
+//             7  +   8  + 10
+// console.log(add);
+
+
+// let num1 = 6;
+// let num2 =3;
+// console.log(num2 - num1);
+
+
+// let num1 = 5;
+// let num2 = 4;
+// console.log(num1 * num2);
+
+
+// let num1 = 20;
+// let num2 = 4;
+// console.log(num1 / num2);
+
+
+// let num1 = 21;
+// let num2 = 4;
+// console.log(num1 % num2);
+
+
+
+// Concatenation
+// let firstName = "Atia";
+// let lastName = "Khan";
+// console.log(firstName + " " + lastName);
+
+// let num1 = 10
+// let num2 = "20"
+// console.log(num1 + num2);
+
+
+// let n =5;
+// n = n + 6;
+// n += 6;
+
+// n -= 1;
+// console.log(n);
+
+
+
+
+// ----------------- Comparison Operators ---------------
+
+// let n = 5;
+// let a = "5";
+// console.log(n == a);
+
+
+// let n = 5;
+// let a = "5";
+// console.log(n === a);
+
+
+// let num = 10;
+// console.log(num < 5);
+//             10  < 5
+
+
+// let num = 10;
+// console.log(num > 5);
+//             10  > 5
+
+
+// let num = 10;
+// console.log(num >= 5);
+//             10  >= 5
+
+
+// let num = 10;
+// console.log(num <= 5);
+//             10  <= 5
+
+
+// let num = 10;
+// console.log(num != 10);
+//             10  != 10
+
+
+
+
+
+
